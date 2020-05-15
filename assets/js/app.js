ffbb8dfc980176e1cf061227524722d71d9e5074
Drew Fravert <drew@fravert.dev>
2020-05-15 13:21:43 -0500

    Improve font performance

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,9 @@ import ColorScheme from "./component/color-scheme.js";
 // import Tabs from "./component/tabs.js";
 
 // js vendors
-import FontAwesome from "@fortawesome/fontawesome-pro/js/all.js";
+import FontAwesomeSolids from "@fortawesome/fontawesome-pro/js/solid.js";
+import FontAwesomeBrands from "@fortawesome/fontawesome-pro/js/brands.js";
+import FontAwesome from "@fortawesome/fontawesome-pro/js/fontawesome.js";
 
 /*
 ==========================================================================================
